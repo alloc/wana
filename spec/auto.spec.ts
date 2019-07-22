077787ca4d4b4dd1ed05bd9d1b773d0cdeb20a38
Alec Larson <alec.stanford.larson@gmail.com>
2019-07-22 16:32:54 -0400

    test: update "auto" tests

--- spec/auto.spec.ts
+++ spec/auto.spec.ts
@@ -6,13 +6,15 @@ function expectRuns(n: number) {
   expect(runs).toBe(prevRuns + n)
   prevRuns = runs
 }
+
 let ctx = o({
   effect: () => {},
 })
-let _ = auto(() => {
-  ctx.effect()
-  runs++
-})
+
+// Synchronously run the effect when it changes.
+auto(() => (ctx.effect(), runs++), { delay: 0 })
+
+// Replace the effect and reset the run count.
 let use = (effect: () => void) => {
   ctx.effect = effect
   prevRuns = runs = 0
@@ -84,73 +86,6 @@ describe('auto()', () => {
     beforeEach(() => {
       arr = o([])
     })
-    function ensureReversed() {
-      const old = [...arr]
-      arr.reverse()
-      expect(arr).not.toEqual(old)
-    }
-    function ensureSorted() {
-      const old = [...arr]
-      arr.sort()
-      expect(arr).not.toEqual(old)
-    }
-    test('.forEach()', () => {
-      use(() => arr.forEach(() => {}))
-
-      arr.pop() // empty pop
-      expectRuns(0)
-
-      arr.push(1, 2) // push many
-      expectRuns(1)
-
-      arr.pop() // pop one
-      expectRuns(1)
-
-      arr.unshift(1, 2) // unshift many
-      expectRuns(1)
-
-      arr.splice(0, 0) // empty splice
-      expectRuns(0)
-
-      arr.splice(1, 1) // remove one
-      expectRuns(1)
-
-      arr.splice(2, 0, 3, 4) // insert two
-      expectRuns(1)
-
-      arr.splice(1, 1, 3) // remove one, insert one
-      expectRuns(1)
-
-      arr.shift() // shift one
-      expectRuns(1)
-
-      ensureReversed() // not reversed yet
-      expectRuns(1)
-
-      ensureSorted() // not sorted yet
-      expectRuns(1)
-
-      arr.sort() // already sorted
-      expectRuns(1)
-
-      arr.length = 0 // truncate to length of 0
-      expectRuns(1)
-
-      arr.length = 2 // expand to length of 2
-      expectRuns(0)
-
-      arr[2] = 1 // set new index
-      expectRuns(1)
-
-      arr[1] = 1 // fill hole
-      expectRuns(1)
-
-      arr[1] = 2 // set old index
-      expectRuns(1)
-
-      arr[1] = 2 // set old index (no change)
-      expectRuns(0)
-    })
     test('.length', () => {
       use(() => arr.length)
 
@@ -205,6 +140,90 @@ describe('auto()', () => {
       arr[1] = 2 // set old index (no change)
       expectRuns(0)
     })
+    test('.concat()', () => {
+      const left = arr
+      const right: any[] = o([])
+      use(() => left.concat(right))
+
+      left.push(1)
+      expectRuns(1)
+
+      right.push(1)
+      expectRuns(1)
+    })
+    test('.forEach()', () => {
+      use(() => arr.forEach(() => {}))
+      wholeMutations()
+    })
+    test('.keys()', () => {
+      use(() => arr.keys())
+      wholeMutations()
+    })
+    function ensureReversed() {
+      const old = [...arr]
+      arr.reverse()
+      expect(arr).not.toEqual(old)
+    }
+    function ensureSorted() {
+      const old = [...arr]
+      arr.sort()
+      expect(arr).not.toEqual(old)
+    }
+    function wholeMutations() {
+      arr.pop() // empty pop
+      expectRuns(0)
+
+      arr.push(1, 2) // push many
+      expectRuns(1)
+
+      arr.pop() // pop one
+      expectRuns(1)
+
+      arr.unshift(1, 2) // unshift many
+      expectRuns(1)
+
+      arr.splice(0, 0) // empty splice
+      expectRuns(0)
+
+      arr.splice(1, 1) // remove one
+      expectRuns(1)
+
+      arr.splice(2, 0, 3, 4) // insert two
+      expectRuns(1)
+
+      arr.splice(1, 1, 3) // remove one, insert one
+      expectRuns(1)
+
+      arr.shift() // shift one
+      expectRuns(1)
+
+      ensureReversed() // not reversed yet
+      expectRuns(1)
+
+      ensureSorted() // not sorted yet
+      expectRuns(1)
+
+      arr.sort() // already sorted
+      expectRuns(1)
+
+      arr.length = 0 // truncate to length of 0
+      expectRuns(1)
+
+      arr.length = 2 // expand to length of 2
+      expectRuns(0)
+
+      arr[2] = 1 // set new index
+      expectRuns(1)
+
+      arr[1] = 1 // fill hole
+      expectRuns(1)
+
+      arr[1] = 2 // set old index
+      expectRuns(1)
+
+      arr[1] = 2 // set old index (no change)
+      expectRuns(0)
+    }
   })
   describe('sets', () => {
     let set: Set<any>
@@ -266,7 +285,47 @@ describe('auto()', () => {
       map.set(1, 2)
     })
     const key = {}
-    test.todo('.has()')
+    test('.has()', () => {
+      use(() => map.has(key))
+
+      map.set(key, 1) // add our key
+      expectRuns(1)
+
+      map.set(key, 1) // set our key (no change)
+      expectRuns(0)
+
+      map.set(key, 2) // set our key
+      expectRuns(1)
+
+      map.delete(key) // delete our key
+      expectRuns(1)
+
+      map.set(0, 1) // add other key
+      expectRuns(0)
+
+      map.set(0, 1) // set other key (no change)
+      expectRuns(0)
+
+      map.set(0, 2) // set other key
+      expectRuns(0)
+
+      map.delete(0) // delete other key
+      expectRuns(0)
+
+      expect(map.size).not.toBe(0)
+      map.clear() // clear other values
+      expectRuns(0)
+
+      expect(map.size).toBe(0)
+      map.clear() // clear when empty
+      expectRuns(0)
+
+      map.set(key, 1) // re-add our key for next test
+      expectRuns(1)
+
+      map.clear() // clear our value
+      expectRuns(1)
+    })
     test('.get()', () => {
       use(() => map.get(key))
 
