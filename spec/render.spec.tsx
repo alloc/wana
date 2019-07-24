1b0940e769549c97e75f093ae6fa4ce9721fa2bd
Alec Larson <alec.stanford.larson@gmail.com>
2019-07-23 22:23:10 -0400

    test: update "useAuto" tests

--- spec/render.spec.tsx
+++ spec/render.spec.tsx
@@ -11,6 +11,37 @@ beforeAll(() => {
 })
 
 describe('useAuto', () => {
+  it('runs on every render by default', () => {
+    const calls = []
+    const state = o({ count: 0 })
+    const Test = () => {
+      useAuto(() => {
+        calls.push(state.count)
+      })
+      return null
+    }
+
+    const elem = render(<Test />)
+    elem.rerender(<Test />)
+
+    expect(calls).toEqual([0, 0])
+  })
+  it('can be given a deps array', () => {
+    const calls = []
+    const state = o({ count: 0 })
+    const Test = (props: { deps: any[] }) => {
+      useAuto(() => {
+        calls.push(state.count)
+      }, props.deps)
+      return null
+    }
+
+    const elem = render(<Test deps={[1]} />)
+    elem.rerender(<Test deps={[1]} />) // useAuto should *not* run here
+    elem.rerender(<Test deps={[2]} />)
+
+    expect(calls).toEqual([0, 0])
+  })
   it('reacts to observable changes', async () => {
     const calls = []
     const state = o({ count: 0 })
@@ -29,13 +60,14 @@ describe('useAuto', () => {
     await Promise.resolve()
     expect(calls).toEqual([0, 2])
   })
+  it.todo('stops observing on dismount')
   it('can be used inside a "withAuto" component', async () => {
     const calls = []
     const state = o({ count: 0 })
     const Test = withAuto(() => {
       useAuto(() => {
         calls.push(state.count)
-      })
+      }, [])
       return <div>{state.count}</div>
     })
 
