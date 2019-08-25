f990e8213b82de16e24675f001450bc34c0674ab
Alec Larson <alec.stanford.larson@gmail.com>
2019-08-25 02:04:11 -0400

    feat: let "useO" create observable functions
    Pass a function that returns a function and you get an observable function back.
    
    The observable function is disposed of on dismount,
    and it postpones subscribing to observed values until React's commit phase.

--- src/ui/useO.ts
+++ src/ui/useO.ts
@@ -1,17 +1,43 @@
+import { useEffect } from 'react'
 import { useMemoOne as useMemo } from 'use-memo-one'
 import { emptyArray, isFunction } from '../common'
+import { derive, Derived } from '../derive'
 import { untracked } from '../global'
-import { o } from '../observable'
+import { o } from '../o'
+import { $$ } from '../symbols'
+import { useDispose } from './common'
 
-/** Create observable component state */
-export function useO<T>(create: () => T, deps?: readonly any[]): T
-export function useO<T>(state: Exclude<T, Function>, deps?: readonly any[]): T
-export function useO<T>(
-  state: Exclude<T, Function> | (() => T),
+/** Create a derived function that is managed by React. */
+export function useO<T extends any[], U>(
+  create: () => (...args: T) => U,
   deps?: readonly any[]
-): T {
-  return useMemo(
-    () => o(isFunction(state) ? untracked(state) : state),
+): Derived<T, U>
+
+/** Create observable component state. */
+export function useO<T>(
+  create: () => Exclude<T, Function>,
+  deps?: readonly any[]
+): T
+
+/** Memoize an object and return its observable proxy. Non-objects are returned as-is. */
+export function useO<T>(state: Exclude<T, Function>, deps?: readonly any[]): T
+
+/** @internal */
+export function useO(state: any, deps?: readonly any[]) {
+  let result = useMemo<any>(
+    () => (isFunction(state) ? untracked(state) : state),
     deps || emptyArray
   )
+  if (isFunction(result)) {
+    result = useMemo(() => {
+      const derived = derive(result)
+      derived[$$].lazy = true
+      return derived
+    }, [result])
+    useDispose(result.dispose)
+    useEffect(() => {
+      result[$$].commit()
+    })
+  }
+  return o(result)
 }
