8458a2d43798c4490f8006faf4a7d83829b8fc98
Alec Larson <alec.stanford.larson@gmail.com>
2019-09-27 21:13:33 -0400

    nit: update comment

--- src/no.ts
+++ src/no.ts
@@ -5,6 +5,8 @@ import { $$ } from './symbols'
 /**
  * Get the original object from an observable proxy,
  * or wrap a function to disable observation inside it.
+ *
+ * Read `no` as "non-observable", essentially the reverse of the `o` function.
  */
 export function no<T>(value: T): T {
   if (isFunction(value)) {
