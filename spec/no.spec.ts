640596f3100d6a596352f2a57e0cd17c3b434326
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-02 13:40:32 -0400

    test: add tests for the "no" function

---
+++ spec/no.spec.ts
@@ -0,0 +1,27 @@
+import { no, o } from '../src'
+import { global } from '../src/global'
+
+describe('no()', () => {
+  it('returns the original object when given an observable proxy', () => {
+    const obj = {}
+    const proxy = o(obj)
+    expect(proxy).not.toBe(obj)
+    expect(no(proxy)).toBe(obj)
+  })
+
+  it('returns a wrapped function when given a function', () => {
+    const obj = o({ a: 1 })
+    const fn = jest.fn((inc = 0) => inc + obj.a)
+    const wrap = no(fn)
+    const observe = (global.observe = jest.fn())
+    expect(wrap(1)).toBe(2)
+    expect(observe).not.toHaveBeenCalled()
+    expect(fn).toHaveBeenCalled()
+    fn()
+    expect(observe).toHaveBeenCalled()
+  })
+
+  it('returns the argument as-is when not observable', () => {
+    expect(no(1)).toBe(1)
+  })
+})
