e9e606942339bd73f35eaa1b9dfb5b854a61bb80
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-18 15:20:05 -0400

    build: use "esModuleInterop: true"

--- spec/useChanges.spec.tsx
+++ spec/useChanges.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import * as React from 'react'
+import React from 'react'
 import { $O, Change, o, useChanges } from '../src'
 
 describe('useChanges', () => {
--- spec/useDerived.spec.tsx
+++ spec/useDerived.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import * as React from 'react'
+import React from 'react'
 import { $O, o, useAuto, useDerived } from '../src'
 import { Derived } from '../src/derive'
 
