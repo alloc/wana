733d26627b0768ff01dc5143f0c98ae4ce4ae97e
Alec Larson <alec.stanford.larson@gmail.com>
2019-10-31 16:39:51 -0400

    fix: "withAuto" depth doubling bug

--- src/ui/AutoContext.tsx
+++ src/ui/AutoContext.tsx
@@ -3,10 +3,14 @@ import React, { ReactNode } from 'react'
 const context = React.createContext({ depth: 0 })
 const { Provider } = context
 
-/** The provided state is assumed to be constant (except for children). */
-export const AutoContext = (props: { depth: number; children: ReactNode }) => {
-  const state = React.useRef({ depth: props.depth + 1 }).current
-  return <Provider value={state}>{props.children}</Provider>
+export type AutoContextProps = {
+  depth: number
+  children: ReactNode
 }
+
+/** The provided state is assumed to be constant (except for children). */
+export const AutoContext = ({ children, ...props }: AutoContextProps) => (
+  <Provider value={React.useRef(props).current}>{children}</Provider>
+)
 
 export const useAutoContext = () => React.useContext(context)
