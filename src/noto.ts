1d2483cfbfa61e8adddc62375ca0ea408876cc9a
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-12 12:43:27 -0500

    fix: ensure "global.auto" is null inside "noto"

--- src/noto.ts
+++ src/noto.ts
@@ -2,11 +2,13 @@ import { global } from './global'
 
 /** Run an effect without any observable tracking */
 export function noto<T>(effect: () => T): T {
-  const oldValue = global.observe
+  const { auto, observe } = global
+  global.auto = null
   global.observe = null
   try {
     return effect()
   } finally {
-    global.observe = oldValue
+    global.auto = auto
+    global.observe = observe
   }
 }
