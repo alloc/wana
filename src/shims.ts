17be4087f01471d601134905199604cf3ddfd9ee
Alec Larson <alec.stanford.larson@gmail.com>
2019-11-16 12:48:26 -0500

    nit: return the native impl of an observable shim if no observer exists

--- src/shims.ts
+++ src/shims.ts
@@ -1,5 +1,5 @@
 import { setHidden } from './common'
-import { observe } from './global'
+import { global, observe } from './global'
 import { $$, $O } from './symbols'
 
 const shims: Array<[object, string[]]> = [
@@ -10,14 +10,17 @@ const shims: Array<[object, string[]]> = [
 shims.forEach(([namespace, keys]) =>
   keys.forEach(key => {
     const fn = namespace[key]
-    const shim = (namespace[key] = (...args: any[]) => {
+    const shim = (...args: any[]) => {
       const target = args[0] && args[0][$$]
       if (target) {
         observe(target, $O)
         args[0] = target
       }
       return fn(...args)
-    })
+    }
     setHidden(shim, 'name', fn.name)
+    Object.defineProperty(namespace, key, {
+      get: () => (global.observe ? shim : fn),
+    })
   })
 )
