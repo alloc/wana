486cddf9b3302a2f58b8263f8eee37f9d5b43ed0
Alec Larson <alec.stanford.larson@gmail.com>
2019-12-17 14:39:14 -0500

    test: use react-dom unstable_batchedUpdates

--- spec/.setup.ts
+++ spec/.setup.ts
@@ -1,5 +1,9 @@
 import { act } from '@testing-library/react'
+import { unstable_batchedUpdates } from 'react-dom'
 import { batch } from '../src/batch'
+import { global } from '../src/global'
+
+global.batchedUpdates = unstable_batchedUpdates
 
 const { render } = batch
 batch.render = (depth, effect) => {
