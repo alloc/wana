4a67e4f5b7c4e317e68a6807b606c12e6bedbdf6
Alec Larson <alec.stanford.larson@gmail.com>
2019-12-17 14:40:01 -0500

    test: wrap Auto#run effects with "act"

--- spec/.setup.ts
+++ spec/.setup.ts
@@ -1,5 +1,6 @@
 import { act } from '@testing-library/react'
 import { unstable_batchedUpdates } from 'react-dom'
+import { Auto } from '../src/auto'
 import { batch } from '../src/batch'
 import { global } from '../src/global'
 
@@ -8,4 +9,15 @@ global.batchedUpdates = unstable_batchedUpdates
 const { render } = batch
 batch.render = (depth, effect) => {
   render(depth, () => act(effect))
+}
+
+const { run } = Auto.prototype
+Auto.prototype.run = function(compute) {
+  return run.call(this, () => {
+    let result: any
+    act(() => {
+      result = compute()
+    })
+    return result
+  })
 }
