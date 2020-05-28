98b806190f2b22c24c9bc4979a711e3ed19abc06
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-05-27 21:49:09 -0400

    feat(useAuto): add overload for memoized effects
    By "memoized effect", I mean the `effect` only runs when the `compute` function returns a new value. And the compute function only runs when one of its observed values is changed.
    
    When `deps` are changed, the memoized state is reset.

--- src/ui/useAuto.ts
+++ src/ui/useAuto.ts
@@ -1,11 +1,44 @@
+import { is } from '@alloc/is'
 import { useEffect } from 'react'
 import { Auto } from '../auto'
+import { no } from '../no'
 import { useConstant, useDispose } from './common'
+import { useDerived } from './useDerived'
+
+type Deps = readonly any[]
+type EffectReturn = void | (() => void | undefined)
 
 /** Wrap a `useEffect` call with magic observable tracking */
-export function useAuto(effect: React.EffectCallback, deps?: readonly any[]) {
+export function useAuto(effect: () => EffectReturn, deps?: Deps): Auto
+
+/**
+ * Combine a `useEffect` call with an `Auto` instance that invokes an
+ * unobserved side `effect` when the `compute` function returns a new value.
+ */
+export function useAuto<T>(
+  compute: () => T,
+  effect: (value: T) => EffectReturn,
+  deps?: Deps
+): Auto
+
+/** @internal */
+export function useAuto(
+  computeOrEffect: () => unknown,
+  effectOrDeps?: Function | Deps,
+  deps?: Deps
+) {
   const auto = useConstant(() => new Auto())
   useDispose(() => auto.dispose())
+
+  let effect: () => EffectReturn
+  if (is.function(effectOrDeps)) {
+    const compute = useDerived(computeOrEffect, deps)
+    effect = () => no(effectOrDeps as any)(compute())
+  } else {
+    effect = computeOrEffect as any
+    deps = effectOrDeps as any
+  }
+
   useEffect(() => auto.run(effect), deps)
   return auto
 }
