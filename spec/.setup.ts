434d5816838fbd849a725376f3e5df13a644da0c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-06-08 18:27:48 -0400

    chore: use @wana/test-utils

--- spec/.setup.ts
+++ spec/.setup.ts
@@ -1,23 +1,5 @@
 import { act } from '@testing-library/react'
-import { unstable_batchedUpdates } from 'react-dom'
-import { Auto } from '../src/auto'
-import { batch } from '../src/batch'
-import { globals } from '../src/globals'
+import { setupEnv } from '@wana/test-utils'
+import { globals } from '../src'
 
-globals.batchedUpdates = unstable_batchedUpdates
-
-const { render } = batch
-batch.render = (depth, effect) => {
-  render(depth, () => act(effect))
-}
-
-const { run } = Auto.prototype
-Auto.prototype.run = function(compute) {
-  return run.call(this, () => {
-    let result: any
-    act(() => {
-      result = compute()
-    })
-    return result
-  })
-}
+setupEnv({ act, globals })
