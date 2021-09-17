7d6eb5751cdeba57c0c6c537d338f19dca066bff
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-09-16 21:06:04 -0400

    feat: add key argument to `watch` function
    Watch a single key in an observable object. If the value is observable, watch it recursively.

--- src/watch.ts
+++ src/watch.ts
@@ -1,14 +1,49 @@
 import { is } from '@alloc/is'
-import { Change, ObservedSlot, Observer, ObserverTarget } from './observable'
+import {
+  Change,
+  Observable,
+  ObservedSlot,
+  Observer,
+  ObserverTarget,
+} from './observable'
 import { $O } from './symbols'
 
 type WatchedState = ObserverTarget & {
   forEach?: (cb: (value: any, key: any, ctx: any) => void) => void
 }
 
-/** Watch an observable tree for changes */
-export function watch(root: object, onChange: (change: Change) => void) {
-  return new Watcher(root, onChange)
+type ChangeHandler = (change: Change) => void
+
+/**
+ * Watch a single property.
+ * If its value is observable, watch it recursively.
+ */
+export function watch<P>(
+  root: Map<P, any>,
+  key: P,
+  onChange: (change: Change) => void
+): Watcher
+
+export function watch<T extends object, P extends string & keyof T>(
+  root: T,
+  key: P,
+  onChange: (change: Change) => void
+): Watcher
+
+/**
+ * Watch an observable tree for changes.
+ * Only observable objects are searched for watchable values.
+ */
+export function watch(root: object, onChange: ChangeHandler): Watcher
+
+export function watch(
+  root: object & { [$O]?: Observable },
+  arg2: unknown,
+  arg3?: ChangeHandler
+) {
+  return arg3
+    ? new Watcher(root, arg3, arg2)
+    : new Watcher(root, arg2 as ChangeHandler)
 }
 
 /** An observer of deep changes */
@@ -16,7 +51,11 @@ export class Watcher extends Observer {
   observed = new Set<ObservedSlot>()
   counts = new Map<object, number>()
 
-  constructor(readonly root: object, onChange: (change: Change) => void) {
+  constructor(
+    readonly root: object,
+    onChange: ChangeHandler,
+    readonly key?: any
+  ) {
     super()
     this.onChange = change => {
       const { op, target, key, value, oldValue } = change
@@ -36,7 +75,14 @@ export class Watcher extends Observer {
       }
       onChange(change)
     }
-    this._watch(root)
+    if (key) {
+      this.counts.set(root, 1)
+      const observable = root[$O] as Observable
+      this.observed.add(observable.get(key).add(this))
+      this.watch(root[key], key, root)
+    } else {
+      this._watch(root)
+    }
   }
 
   watch = (value: any, key?: any, ctx?: any) => {
