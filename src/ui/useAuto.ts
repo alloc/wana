50c21f53ae047458ef10a0f34fcac15184cc18c9
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-09-25 16:44:34 -0400

    wip

--- src/ui/useAuto.ts
+++ src/ui/useAuto.ts
@@ -2,6 +2,7 @@ import { is } from '@alloc/is'
 import { useEffect } from 'react'
 import { batchedUpdates } from 'react-batched-updates'
 import { Auto } from '../auto'
+import { Falsy } from '../common'
 import { no } from '../no'
 import { useConstant, useDispose } from './common'
 import { useDerived } from './useDerived'
@@ -12,6 +13,12 @@ type EffectReturn = void | (() => void | undefined)
 /** Wrap a `useEffect` call with magic observable tracking */
 export function useAuto(effect: () => EffectReturn, deps?: Deps): Auto
 
+/** Wrap a `useEffect` call with magic observable tracking */
+export function useAuto(
+  effect: (() => EffectReturn) | Falsy,
+  deps?: Deps
+): Auto | null
+
 /**
  * Combine a `useEffect` call with an `Auto` instance that invokes an
  * unobserved side `effect` when the `compute` function returns a new value.
@@ -21,6 +28,16 @@ export function useAuto<T>(
   effect: (value: T) => EffectReturn,
   deps?: Deps
 ): Auto
+
+/**
+ * Combine a `useEffect` call with an `Auto` instance that invokes an
+ * unobserved side `effect` when the `compute` function returns a new value.
+ */
+export function useAuto<T>(
+  compute: (() => T) | Falsy,
+  effect: (value: T) => EffectReturn,
+  deps?: Deps
+): Auto | null
 
 /** @internal */
 export function useAuto(
