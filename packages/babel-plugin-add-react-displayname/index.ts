5252bb5ed1619aa75229074e0b94f5e9a2ba2c16
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-17 15:35:00 -0400

    fix(add-react-displayname): resolve typescript issues

--- packages/babel-plugin-add-react-displayname/index.ts
+++ packages/babel-plugin-add-react-displayname/index.ts
@@ -1,154 +1,155 @@
+import type { NodePath, PluginPass, types as t } from '@babel/core'
 import { declare } from '@babel/helper-plugin-utils'
-import { types as t, ConfigAPI, PluginObj, NodePath } from '@babel/core'
 
 type Options = {
   callees?: string[]
 }
 
-export default declare(
-  (api: ConfigAPI): PluginObj => {
-    api.assertVersion(7)
-    const FunctionVisitor: babel.Visitor<any> = {
-      FunctionExpression(path, state) {
-        setDisplayNameIfPossible(path, state)
+export default declare(babel => {
+  babel.assertVersion(7)
+
+  const { types: t } = babel
+
+  const FunctionVisitor: babel.Visitor<PluginPass> = {
+    FunctionExpression(path, state) {
+      setDisplayNameIfPossible(path, state)
+    },
+    ArrowFunctionExpression(path, state) {
+      setDisplayNameIfPossible(path, state)
+    },
+  }
+
+  return {
+    name: '@wana/babel-plugin-add-react-displayname',
+    visitor: {
+      VariableDeclaration(path, state) {
+        path.traverse(FunctionVisitor, state)
       },
-      ArrowFunctionExpression(path, state) {
-        setDisplayNameIfPossible(path, state)
-      },
-    }
-    return {
-      name: '@wana/babel-plugin-add-react-displayname',
-      visitor: {
-        VariableDeclaration(path, state) {
-          path.traverse(FunctionVisitor, state)
-        },
-      },
-    }
+    },
   }
-)
 
-function setDisplayNameIfPossible(
-  path: NodePath<t.FunctionExpression | t.ArrowFunctionExpression>,
-  state: { opts: Options }
-) {
-  const prevStmt = findBlockChild(path)
-  if (!prevStmt) return
-
-  const displayName = inferDisplayName(path, state.opts)
-  if (!displayName || /^[a-z]/.test(displayName)) return
-
-  if (path.isArrowFunctionExpression()) {
-    const { params, body, returnType, typeParameters } = path.node
-    const bodyBlock = t.isExpression(body)
-      ? t.blockStatement([t.returnStatement(body)])
-      : body
-    const functionExpr: Parameters<typeof t.functionExpression> = [
-      t.identifier(displayName),
-      params,
-      bodyBlock,
-      false,
-      false,
-    ]
-    if (returnType || typeParameters) {
-      functionExpr.push(returnType, typeParameters)
-    }
-    path.replaceWith(t.functionExpression(...functionExpr))
-  } else {
-    const newStmt = t.expressionStatement(
-      t.assignmentExpression(
-        '=',
-        t.memberExpression(
-          t.identifier(displayName),
-          t.identifier('displayName')
-        ),
-        t.stringLiteral(displayName)
-      )
-    )
-    newStmt.trailingComments = prevStmt.node.trailingComments
-    prevStmt.node.trailingComments = null
-    prevStmt.insertAfter(newStmt)
-  }
-}
-
-function findLeftValue(expr: NodePath<t.Expression>) {
-  let path = expr.parentPath
-  while (path) {
-    if (path.isBlockParent() || path.isProperty()) {
-      return
-    }
-    if (path.isAssignmentExpression()) {
-      return path.node.left
-    }
-    if (path.isVariableDeclarator()) {
-      return path.node.id
-    }
-    path = path.parentPath
-  }
-}
-
-function findBlockChild(path: NodePath) {
-  while (path.parentPath) {
-    if (path.parentPath.isProperty()) {
-      return
-    }
-    if (path.parentPath.isBlockParent()) {
-      return path
-    }
-    path = path.parentPath
-  }
-}
-
-function inferDisplayName(
-  funcPath: NodePath<t.FunctionExpression | t.ArrowFunctionExpression>,
-  opts: Options
-) {
-  const parentNode = funcPath.parentPath.node
-  if (t.isVariableDeclarator(parentNode)) {
-    // Ignore plain function components, like "const Foo = () => { ... }"
-    return null
-  }
-  if (t.isCallExpression(parentNode) && parentNode.callee == funcPath.node) {
-    // Ignore IIFEs that return JSX, like "(() => { ... })()"
-    return null
-  }
-  if (
-    doesReturnJSX(funcPath.node.body) ||
-    (opts.callees && opts.callees.some(callee => hasCallee(funcPath, callee)))
+  function setDisplayNameIfPossible(
+    path: NodePath<t.FunctionExpression | t.ArrowFunctionExpression>,
+    state: PluginPass
   ) {
-    const left = findLeftValue(funcPath)
-    if (t.isIdentifier(left)) {
-      return left.name
+    const prevStmt = findBlockChild(path)
+    if (!prevStmt) return
+
+    const displayName = inferDisplayName(path, state.opts as Options)
+    if (!displayName || /^[a-z]/.test(displayName)) return
+
+    if (path.isArrowFunctionExpression()) {
+      const { params, body, returnType, typeParameters } = path.node
+      const bodyBlock = t.isExpression(body)
+        ? t.blockStatement([t.returnStatement(body)])
+        : body
+      const functionExpr = t.functionExpression(
+        t.identifier(displayName),
+        params,
+        bodyBlock,
+        false,
+        false
+      )
+      functionExpr.returnType = returnType
+      functionExpr.typeParameters = typeParameters
+      path.replaceWith(functionExpr)
+    } else {
+      const newStmt = t.expressionStatement(
+        t.assignmentExpression(
+          '=',
+          t.memberExpression(
+            t.identifier(displayName),
+            t.identifier('displayName')
+          ),
+          t.stringLiteral(displayName)
+        )
+      )
+      newStmt.trailingComments = prevStmt.node.trailingComments
+      prevStmt.node.trailingComments = null
+      prevStmt.insertAfter(newStmt)
     }
   }
-  return null
-}
 
-function hasCallee(path: NodePath, callee: string) {
-  path = path.parentPath
-  while (path.isCallExpression()) {
-    if (t.isIdentifier(path.node.callee, { name: callee })) {
-      return true
+  function findLeftValue(expr: NodePath<t.Expression>) {
+    let path = expr.parentPath
+    while (path) {
+      if (path.isBlockParent() || path.isProperty()) {
+        return
+      }
+      if (path.isAssignmentExpression()) {
+        return path.node.left
+      }
+      if (path.isVariableDeclarator()) {
+        return path.node.id
+      }
+      path = path.parentPath!
     }
-    path = path.parentPath
-  }
-  return false
-}
-
-function doesReturnJSX(node: t.Expression | t.BlockStatement) {
-  if (!node) return false
-  if (t.isJSX(node)) {
-    return true
   }
 
-  if (t.isBlockStatement(node)) {
-    const len = node.body.length
-    if (len) {
-      const lastNode = node.body[len - 1]
-      if (t.isReturnStatement(lastNode)) {
-        return lastNode.argument && t.isJSX(lastNode.argument)
+  function findBlockChild(path: NodePath) {
+    while (path.parentPath) {
+      if (path.parentPath.isProperty()) {
+        return
+      }
+      if (path.parentPath.isBlockParent()) {
+        return path
+      }
+      path = path.parentPath
+    }
+  }
+
+  function inferDisplayName(
+    funcPath: NodePath<t.FunctionExpression | t.ArrowFunctionExpression>,
+    opts: Options
+  ) {
+    const parentNode = funcPath.parentPath.node
+    if (t.isVariableDeclarator(parentNode)) {
+      // Ignore plain function components, like "const Foo = () => { ... }"
+      return null
+    }
+    if (t.isCallExpression(parentNode) && parentNode.callee == funcPath.node) {
+      // Ignore IIFEs that return JSX, like "(() => { ... })()"
+      return null
+    }
+    if (
+      doesReturnJSX(funcPath.node.body) ||
+      (opts.callees && opts.callees.some(callee => hasCallee(funcPath, callee)))
+    ) {
+      const left = findLeftValue(funcPath)
+      if (t.isIdentifier(left)) {
+        return left.name
       }
     }
+    return null
   }
 
-  return false
-}
+  function hasCallee(path: NodePath, callee: string) {
+    path = path.parentPath!
+    while (path.isCallExpression()) {
+      if (t.isIdentifier(path.node.callee, { name: callee })) {
+        return true
+      }
+      path = path.parentPath
+    }
+    return false
+  }
+
+  function doesReturnJSX(node: t.Expression | t.BlockStatement) {
+    if (!node) return false
+    if (t.isJSX(node)) {
+      return true
+    }
+
+    if (t.isBlockStatement(node)) {
+      const len = node.body.length
+      if (len) {
+        const lastNode = node.body[len - 1]
+        if (t.isReturnStatement(lastNode)) {
+          return lastNode.argument && t.isJSX(lastNode.argument)
+        }
+      }
+    }
+
+    return false
+  }
+})
