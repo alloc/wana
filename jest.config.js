5b78402ad2c5a93d528013ed77ab7e19165521c2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-11-13 15:15:24 -0500

    test: use source files instead of build artifacts

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['packages'],
   setupFiles: ['<rootDir>/spec/.setup.ts'],
+  moduleNameMapper: {
+    '^wana$': '<rootDir>/src/index.ts',
+    '^wana/(.+)$': '<rootDir>/src/$1.ts',
+  },
   transform: {
     '\\.tsx?$': ['esbuild-jest'],
   },
